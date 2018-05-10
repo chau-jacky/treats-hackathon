abc11884f8a3755fb973cbc81c1bbe42e1d6d981
Jacky Chau <jackychau@maplequad.com>
2018-05-10 23:05:37 +0800

    update build version to v5

--- treats-euc-ui/build.sh
+++ treats-euc-ui/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mvn clean package
 
-docker build -t gcr.io/techfest-hackathon-4/treats-euc-ui:v4 .
+docker build -t gcr.io/techfest-hackathon-4/treats-euc-ui:v5 .
 
-gcloud docker -- push gcr.io/techfest-hackathon-4/treats-euc-ui:v4
+gcloud docker -- push gcr.io/techfest-hackathon-4/treats-euc-ui:v5
 
-kubectl set image deployment/treats-euc-ui treats-euc-ui=gcr.io/techfest-hackathon-4/treats-euc-ui:v4
+kubectl set image deployment/treats-euc-ui treats-euc-ui=gcr.io/techfest-hackathon-4/treats-euc-ui:v5
