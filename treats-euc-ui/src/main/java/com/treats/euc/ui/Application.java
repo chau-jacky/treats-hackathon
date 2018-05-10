e3b9615ec526d3f8b11bdbe8034974e9910fc93a
kelcmh <kelcmh@gmail.com>
2018-05-10 20:21:42 +0800

    Test comment

--- treats-euc-ui/src/main/java/com/treats/euc/ui/Application.java
+++ treats-euc-ui/src/main/java/com/treats/euc/ui/Application.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
+	/* Test Comment */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
