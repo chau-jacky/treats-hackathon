9657d19ab384429b46501245f9bb3209d0b8f674
simonchau <pwchau@gmail.com>
2018-05-11 12:13:43 +0800

    Update of EucFlow object and Introduction of EucFlowServices

--- treats-euc-model/src/main/java/com/treats/euc/model/EucSchedule.java
+++ treats-euc-model/src/main/java/com/treats/euc/model/EucSchedule.java
@@ -5,9 +5,31 @@ import java.time.LocalTime;
 public class EucSchedule {
 
 	private LocalTime exectueTime;
-	
 	private String executeFrequency;
 	private String executeDayOfWeek;
 	
+	public LocalTime getExectueTime() {
+		return exectueTime;
+	}
+	
+	public void setExectueTime(LocalTime exectueTime) {
+		this.exectueTime = exectueTime;
+	}
+	
+	public String getExecuteFrequency() {
+		return executeFrequency;
+	}
+	
+	public void setExecuteFrequency(String executeFrequency) {
+		this.executeFrequency = executeFrequency;
+	}
+	
+	public String getExecuteDayOfWeek() {
+		return executeDayOfWeek;
+	}
+	
+	public void setExecuteDayOfWeek(String executeDayOfWeek) {
+		this.executeDayOfWeek = executeDayOfWeek;
+	}
 	
 }
