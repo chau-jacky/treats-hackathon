277203b258d6a1a86fb071b21ce503760a66e1fb
DarwinWu <39133465+DarwinWu@users.noreply.github.com>
2018-05-13 18:29:44 +0800

    PDFGenerator updates

--- treats-euc-ui/src/main/java/com/treats/euc/pdf/PdfGenerator.java
+++ treats-euc-ui/src/main/java/com/treats/euc/pdf/PdfGenerator.java
@@ -1,13 +1,17 @@
 package com.treats.euc.pdf;
 
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import java.io.ByteArrayInputStream;
 //import java.io.OutputStream;
@@ -38,18 +42,23 @@ public class PdfGenerator {
 
 	private static final Boolean False = null;
 	private static Font catFont = new Font(Font.FontFamily.HELVETICA, 28, Font.BOLD);
-//	private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 28, Font.NORMAL, BaseColor.RED);
-//	private static Font greemFont = new Font(Font.FontFamily.HELVETICA, 28, Font.NORMAL, BaseColor.GREEN);
+	// private static Font redFont = new Font(Font.FontFamily.TIMES_ROMAN, 28,
+	// Font.NORMAL, BaseColor.RED);
+	// private static Font greemFont = new Font(Font.FontFamily.HELVETICA, 28,
+	// Font.NORMAL, BaseColor.GREEN);
 
 	private static Font time_Font = new Font(Font.FontFamily.HELVETICA, 12, Font.ITALIC);
 	private static Font chaptor_Pass_Font = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD);
-//	private static Font chaptor_Fail_Font = new Font(Font.FontFamily.HELVETICA, 14, Font.BOLD, BaseColor.RED);
+	// private static Font chaptor_Fail_Font = new Font(Font.FontFamily.HELVETICA,
+	// 14, Font.BOLD, BaseColor.RED);
 
 	private static Font normal_Font = new Font(Font.FontFamily.HELVETICA, 10, Font.NORMAL);
-//	private static Font normal_Red_Font = new Font(Font.FontFamily.HELVETICA, 10, Font.NORMAL, BaseColor.RED);
-//	private static Font normal_Bold_Red_Font = new Font(Font.FontFamily.HELVETICA, 10, Font.BOLD, BaseColor.RED);
+	// private static Font normal_Red_Font = new Font(Font.FontFamily.HELVETICA, 10,
+	// Font.NORMAL, BaseColor.RED);
+	// private static Font normal_Bold_Red_Font = new
+	// Font(Font.FontFamily.HELVETICA, 10, Font.BOLD, BaseColor.RED);
 
-	private String outputMethod;
+	private String outputMethod = "EMAIL";
 	private String fullFileName;
 	private String pdfContent;
 	private String pdfHeader;
@@ -123,17 +132,25 @@ public class PdfGenerator {
 		System.out.println("Generating report...");
 
 		Document doc = new Document(PageSize.A4);
-//		OutputStream file = new FileOutputStream(new File(fullFileName));
-//		PdfWriter writer = PdfWriter.getInstance(doc, file);
 		InputStream is = new ByteArrayInputStream(pdfContent.getBytes());
+		//OutputStream file = null;
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PdfWriter writer = PdfWriter.getInstance(doc, baos);
+		
+		//if (outputMethod.equals("EMAIL")) {
+		is = new ByteArrayInputStream(pdfContent.getBytes());
+		baos = new ByteArrayOutputStream();
+		writer = PdfWriter.getInstance(doc, baos);
+		//} else if (outputMethod.equals("SERVER")) {
+		//	file = new FileOutputStream(new File(fullFileName));
+	    //	writer = PdfWriter.getInstance(doc, file);
+		//}
 		
 		Paragraph p = new Paragraph("Monthly Statement", catFont);
 		p.setAlignment(Element.ALIGN_CENTER);
 
 		doc.open();
-		
+
 		// Create and add an Image
 		Image img = Image.getInstance(headerImage);
 		img.scaleToFit(PageSize.A4.getWidth(), img.getScaledHeight());
@@ -141,72 +158,159 @@ public class PdfGenerator {
 		doc.add(img);
 
 		addEmptyLIne(doc, 3);
-		//doc.add(p);
+		// doc.add(p);
 
-		//addEmptyLIne(doc, 1);
-		//p = new Paragraph(pdfContent, chaptor_Pass_Font);
-		//doc.add(p);
+		// addEmptyLIne(doc, 1);
+		// p = new Paragraph(pdfContent, chaptor_Pass_Font);
+		// doc.add(p);
 		XMLWorkerHelper.getInstance().parseXHtml(writer, doc, is);
-		
+
 		img = Image.getInstance(barcodeImage);
 		img.scaleToFit(200, 500);
 		img.setAbsolutePosition(PageSize.A4.getWidth() - PageSize.A4.getWidth() / 3, 0);
 		doc.add(img);
 
 		doc.close();
-//		file.close();
+		// file.close();
 
 		System.out.println("Report generation completed!");
-		
+
 		return baos;
 	}
-	
+
 	public void generatePdf(ArrayList<String> PdfHtmlList) throws DocumentException, IOException, MessagingException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		for (int i = 0; i < PdfHtmlList.size(); i++) {
 			pdfContent = PdfHtmlList.get(i);
 			baos = generatePdf();
-			
-			//send email statement
+
+			// send email statement
 			EmailDeliveryServices sender = new EmailDeliveryServices();
 			sender.sendEmailWithPdfAndDefaultSetup(baos);
 		}
 	}
 
-/*	public Metadata generatePdfMetadata() throws IOException, TikaException, SAXException, DocumentException {
-		Metadata metadata = new Metadata();
+	public void generatePdf(ArrayList<String> PdfHtmlList, String email)
+			throws DocumentException, IOException, MessagingException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		for (int i = 0; i < PdfHtmlList.size(); i++) {
+			pdfContent = PdfHtmlList.get(i);
+			baos = generatePdf();
 
-		generatePdf();
-		
-		BodyContentHandler handler = new BodyContentHandler();
-		FileInputStream inputstream = new FileInputStream(new File(fullFileName));
-		ParseContext pcontext = new ParseContext();
+			// send email statement
+			EmailDeliveryServices sender = new EmailDeliveryServices();
+			// TODO: sender.sendEmailWithPdfAndDefaultSetup(baos, email);
+		}
+	}
 
-		// parsing the document using PDF parser
-		PDFParser pdfparser = new PDFParser();
-		pdfparser.parse(inputstream, handler, metadata, pcontext);
+	public void generatePdf(ArrayList<String> PdfHtmlList, HttpServletRequest request, HttpServletResponse response)
+			throws DocumentException, IOException, MessagingException {
+		ByteArrayOutputStream baos = null;
+		OutputStream os = null;
 
-		// getting the content of the document
-		System.out.println("Contents of the PDF :" + handler.toString());
+		for (int i = 0; i < PdfHtmlList.size(); i++) {
 
-		// getting metadata of the document
-		System.out.println("Metadata of the PDF:");
-		String[] metadataNames = metadata.names();
+			baos = new ByteArrayOutputStream();
+			pdfContent = PdfHtmlList.get(i);
+			// baos = new ByteArrayOutputStream();
+			baos = generatePdf();
 
-		for (String name : metadataNames) {
-			System.out.println(name + " : " + metadata.get(name));
+			// setting some response headers
+			response.setHeader("Expires", "0");
+			response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
+			response.setHeader("Pragma", "public");
+			// setting the content type
+			response.setContentType("application/pdf");
+			// the contentlength
+			response.setContentLength(baos.size());
+			// write ByteArrayOutputStream to the ServletOutputStream
+			os = response.getOutputStream();
+			baos.writeTo(os);
+			os.flush();
+			os.close();
 		}
 
-		return metadata;
-	}*/
+	}
+
+	/*
+	 * public Metadata generatePdfMetadata() throws IOException, TikaException,
+	 * SAXException, DocumentException { Metadata metadata = new Metadata();
+	 * 
+	 * generatePdf();
+	 * 
+	 * BodyContentHandler handler = new BodyContentHandler(); FileInputStream
+	 * inputstream = new FileInputStream(new File(fullFileName)); ParseContext
+	 * pcontext = new ParseContext();
+	 * 
+	 * // parsing the document using PDF parser PDFParser pdfparser = new
+	 * PDFParser(); pdfparser.parse(inputstream, handler, metadata, pcontext);
+	 * 
+	 * // getting the content of the document
+	 * System.out.println("Contents of the PDF :" + handler.toString());
+	 * 
+	 * // getting metadata of the document
+	 * System.out.println("Metadata of the PDF:"); String[] metadataNames =
+	 * metadata.names();
+	 * 
+	 * for (String name : metadataNames) { System.out.println(name + " : " +
+	 * metadata.get(name)); }
+	 * 
+	 * return metadata; }
+	 */
 
 	static void addEmptyLIne(Document doc, int number) throws DocumentException {
 		for (int i = 0; i < number; i++) {
 			doc.add(new Paragraph(" ", chaptor_Pass_Font));
 		}
 	}
+	
+	
+	public void generatePdfToLocal() throws DocumentException, IOException {
 
+		System.out.println("Generating report...");
+
+		Document doc = new Document(PageSize.A4);
+		InputStream is = new ByteArrayInputStream(pdfContent.getBytes());
+		OutputStream file = new FileOutputStream(new File(fullFileName));
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PdfWriter writer = PdfWriter.getInstance(doc, file);
+		
+		//is = new ByteArrayInputStream(pdfContent.getBytes());
+		baos = new ByteArrayOutputStream();
+		writer = PdfWriter.getInstance(doc, baos);
+
+		
+		Paragraph p = new Paragraph("Monthly Statement", catFont);
+		p.setAlignment(Element.ALIGN_CENTER);
+
+		doc.open();
+
+		// Create and add an Image
+		Image img = Image.getInstance(headerImage);
+		img.scaleToFit(PageSize.A4.getWidth(), img.getScaledHeight());
+		img.setAbsolutePosition(0, PageSize.A4.getHeight() - img.getScaledHeight());
+		doc.add(img);
+
+		addEmptyLIne(doc, 3);
+		// doc.add(p);
+
+		// addEmptyLIne(doc, 1);
+		// p = new Paragraph(pdfContent, chaptor_Pass_Font);
+		// doc.add(p);
+		XMLWorkerHelper.getInstance().parseXHtml(writer, doc, new StringReader(pdfContent));
+		//XMLWorkerHelper.getInstance().parseXHtml(writer, doc, in);
+
+		img = Image.getInstance(barcodeImage);
+		img.scaleToFit(200, 500);
+		img.setAbsolutePosition(PageSize.A4.getWidth() - PageSize.A4.getWidth() / 3, 0);
+		doc.add(img);
+
+		doc.close();
+		// file.close();
+
+		System.out.println("Report generation completed!");
+	}
 	
 	
-	
+
 }
--- treats-euc-ui/src/main/java/com/treats/euc/ui/controller/ExportControllerPdf.java
+++ treats-euc-ui/src/main/java/com/treats/euc/ui/controller/ExportControllerPdf.java
@@ -1,50 +1,69 @@
 package com.treats.euc.ui.controller;
 
+import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 import javax.mail.MessagingException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
+import com.itextpdf.text.pdf.PdfWriter;
 import com.treats.euc.pdf.PdfGenerator;
 
 @RestController
 @RequestMapping("/treats-euc/Export")
 public class ExportControllerPdf {
 
-	@RequestMapping("/Pdf")
-	public HttpServletResponse sendPDFEmail() throws DocumentException, IOException, MessagingException {
-
-		PdfGenerator pdf = new PdfGenerator();
-		pdf.setPdfContent("<html><body> This is my Project </body></html>");
-
-		ByteArrayOutputStream baos = pdf.generatePdf();
-		// EmailDeliveryServices sender = new EmailDeliveryServices();
-		// sender.sendEmailWithPdfAndDefaultSetup(baos);
-
-		HttpServletResponse response = null;
-
-		// setting some response headers
-		response.setHeader("Expires", "0");
-		response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
-		response.setHeader("Pragma", "public");
-		// setting the content type
-		response.setContentType("application/pdf");
-		// the contentlength
-		// response.setContentLength(baos.size());
-		// write ByteArrayOutputStream to the ServletOutputStream
-		OutputStream os = response.getOutputStream();
-		// baos.writeTo(os);
-		os.flush();
-		os.close();
-
-		return response;
-	}
+	@RequestMapping("/getPDF")
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DocumentException, MessagingException {
+        // Get the text that will be added to the PDF
+        String text = request.getParameter("text");
+        if (text == null || text.trim().length() == 0) {
+            text = "You didn't enter any text.";
+        }
+        
+        PdfGenerator pdfGen = new PdfGenerator();
+                
+        //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        //pdfGen.setPdfContent("<html><body> This is my Project </body></html>");
+        //baos = pdfGen.generatePdf();
+        
+        ArrayList<String> PdfHtmlList = new ArrayList();
+        PdfHtmlList.add("<html><body> This is my Project </body></html>");
+        PdfHtmlList.add("<html><body> This is my Project 2 </body></html>");
+        
+        pdfGen.generatePdf(PdfHtmlList, request, response);
+        
+/*        // setting some response headers
+        response.setHeader("Expires", "0");
+        response.setHeader("Cache-Control",
+                "must-revalidate, post-check=0, pre-check=0");
+        response.setHeader("Pragma", "public");
+        // setting the content type
+        response.setContentType("application/pdf");
+        // the contentlength
+        response.setContentLength(baos.size());
+        // write ByteArrayOutputStream to the ServletOutputStream
+        OutputStream os = response.getOutputStream();
+        baos.writeTo(os);
+        os.flush();
+        os.close();*/
+    }
 
 }
-
