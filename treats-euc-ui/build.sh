ca27c60e19fd189c7e5323eea989a1ab75a62bf2
Jacky Chau <jackychau@maplequad.com>
2018-05-15 11:27:39 +0800

    changes for image

--- treats-euc-ui/build.sh
+++ treats-euc-ui/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mvn -DskipTests clean package
 
-docker build -t gcr.io/techfest-hackathon-4/treats-euc-ui:v20 .
+docker build -t gcr.io/techfest-hackathon-4/treats-euc-ui:v22 .
 
-gcloud docker -- push gcr.io/techfest-hackathon-4/treats-euc-ui:v20
+gcloud docker -- push gcr.io/techfest-hackathon-4/treats-euc-ui:v22
 
-kubectl set image deployment/treats-euc-ui treats-euc-ui=gcr.io/techfest-hackathon-4/treats-euc-ui:v20
+kubectl set image deployment/treats-euc-ui treats-euc-ui=gcr.io/techfest-hackathon-4/treats-euc-ui:v22
